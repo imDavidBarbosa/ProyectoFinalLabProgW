4db5567889b7417f22955b38465830d41d068a25
David Barbosa <davidbarbosa3112@gmail.com>
2023-05-25 23:22:01 -0600

    Realizado la tabla de los pedidos del lado del Admin solo falta la capacidad de la edicion del estado y vista del detalle del pedido

--- pages/admin/dashboardadmin.php
+++ pages/admin/dashboardadmin.php
@@ -8,8 +8,51 @@
         <script type="text/javascript" src="../../script/emergentes.js"></script>
     </head>
     <body>
-       
-    
+        <div id= "contenido">
+        <?php
+            // Establecer la conexión a la base de datos
+            // Conexión a base de datos
+            require("../../database/connection.php");
+
+            // Ejecutar la consulta SQL
+            $sql = "SELECT idventa,fecha,idcliente,ventas.idestatus, descestatus FROM ventas INNER JOIN estatus ON estatus.idestatus = ventas.idestatus ORDER BY fecha";
+            $resultado = mysqli_query($conexion, $sql);
+
+            // Crear la tabla HTML y mostrar los datos
+            echo "<table class='tabladis'>";
+            echo "<tr>
+                <th>ID Venta</th>
+                <th>Fecha</th>
+                <th>ID cliente</th>
+                <th>ID Estatus</th>
+                <th>Detalles</th>
+                <th>Actualizar Estado</th>
+            </tr>";
+            while ($fila = mysqli_fetch_assoc($resultado)) {
+                echo "<tr>";
+                echo "<td>" . $fila['idventa'] . "</td>";
+                echo "<td>" . $fila['fecha'] . "</td>";
+                echo "<td>" . $fila['idcliente'] . "</td>";
+                echo "<td>" . $fila['descestatus'] . "</td>";
+                ?>
+                <td>
+                    <a onclick="openPopup(); return false;">
+                        <i class="fa-solid fa-eye"></i>
+                    </a>
+                </td>
+                <td>
+                    <a onclick="openPopup(); return false;">
+                    <i class="fa-solid fa-pen-to-square"></i></i>
+                    </a>
+                </td>
+                <?php
+            }
+            echo "</table>";
+            // Cierre de la tabla donde se muestran los productos
+
+            // Cerrar la conexión a la base de datos
+            mysqli_close($conexion);
+            ?>
         </div>
     </body>
     <script src="../../script/general.js"></script>
